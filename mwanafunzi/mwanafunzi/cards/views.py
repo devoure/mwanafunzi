63c99aa97cda4c4e47f06ef4f14f9571510734d7
devoure <disguisedsandwich@gmail.com>
2024-03-07 12:24:57 +0300

    Added braintree integration

--- mwanafunzi/mwanafunzi/cards/views.py
+++ mwanafunzi/mwanafunzi/cards/views.py
@@ -1,25 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import braintree
 from django.conf import settings
+from invoice.models import Invoice
 
 # Create your views here.
 gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
 
 def process(request):
+    invoice_id = request.session.get('invoice')['id']
+    invoice = Invoice.objects.get(id=invoice_id)
+
     if request.method == 'POST':
         nonce = request.POST.get('payment_method_nonce', None)
         res = gateway.transaction.sale({
-            'amount': 200,
+            'amount': str(invoice.fee_structure.fee),
             'payment_method_nonce': nonce,
             'options': {
                 'submit_for_settlement':True
                 }
             })
+        if res.is_success:
+            return redirect('braintree-done')
+        else:
+            errors_mes = ""
+            for error in res.errors.deep_errors:
+                print(errors_mes + error.code + error.message)
+            return redirect('braintree-failed')
+    else:
+        token = gateway.client_token.generate()
+        context = {'token':token}
+        return render(request, 'process.html', context)
 
 
 def failed(request):
-    pass
+    return render(request, 'failed.html')
 
 def done(request):
-    pass
-
+    return render(request, 'done.html')
