1cf82f3430adfbb8d8adde7bb2ac17672da0631a
devoure <disguisedsandwich@gmail.com>
2024-03-13 16:16:10 +0300

    Added email send code structure

--- mwanafunzi/mwanafunzi/cards/views.py
+++ mwanafunzi/mwanafunzi/cards/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 import braintree
 from django.conf import settings
 from invoice.models import Invoice
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
 
 # Create your views here.
 gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
@@ -37,3 +40,24 @@ def failed(request):
 
 def done(request):
     return render(request, 'done.html')
+
+def send_email(request):
+    invoice_id = request.session.get('invoice')['id']
+    invoice = Invoice.objects.get(id=invoice_id)
+
+    tempate = render_to_string('cards/email.html', { 'name' : request.user.last_name,
+                                                    'invoice_number' : invoice.id,
+                                                    'course': invoice.fee_structure.course.
+                                                    'year': invoice.fee_structure.year,
+                                                    'semester': invoice.fee_structure.semester,
+                                                    'amount': invoice.fee_structure.fee})
+
+
+    email = EmailMessage(
+            'Dev University Fee Statement',
+            template,
+            settings.EMAIL_HOST_USER,
+            ['mshewaathumanibakari@gmail.com']
+            )
+    email_fail_silently = False
+    email.send()
